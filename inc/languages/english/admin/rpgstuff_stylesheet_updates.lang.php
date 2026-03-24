dbd83f989224d24a60922ba0f80e84221753a7d9
Stack <gambill.staci@gmail.com>
2026-03-24 19:49:54 -0400

    Create rpgstuff_stylesheet_updates.lang.php

---
+++ inc/languages/english/admin/rpgstuff_stylesheet_updates.lang.php
@@ -0,0 +1,8 @@
+<?php
+$l['stylesheets'] = "Check style sheets";
+$l['stylesheets_desc'] = "Are your stylesheets missing after a MyBB upgrade? You can add the default stylesheet again here.";
+$l['stylesheets_table'] = "Find missing stylesheets";
+
+$l['stylesheets_masterstyle'] = "Available in MyBB Master Style";
+$l['stylesheets_add'] = "Add to MyBB Master Style";
+$l['stylesheets_flash'] = "The stylesheet has been successfully added to the MyBB Master Style.";
