e6c9f05afa4e1fd1833ccd3179c27c292a9189e3
Stack <gambill.staci@gmail.com>
2026-03-24 19:49:01 -0400

    Create rpgstuff_plugin_updates.lang.php

---
+++ inc/languages/english/admin/rpgstuff_plugin_updates.lang.php
@@ -0,0 +1,8 @@
+<?php
+$l['plugins'] = "Plugins Update";
+$l['plugins_desc'] = "A new version of an RPG plugin has been uploaded and an update is needed? You can do that here.";
+$l['plugins_table'] = "Find plugins to update.";
+
+$l['plugins_actual'] = "on the latest version.";
+$l['plugins_update'] = "Run update.";
+$l['plugins_flash'] = "The update was successfully completed.";
