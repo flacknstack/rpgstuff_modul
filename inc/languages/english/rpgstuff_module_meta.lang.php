ce0b39c21c8d95274859a730288e4c8ed681bcfa
Stack <gambill.staci@gmail.com>
2026-03-24 19:47:05 -0400

    Create rpgstuff_module_meta.lang.php

---
+++ inc/languages/english/rpgstuff_module_meta.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$l['rpg_stuff'] = "RPG Extensions";
+
+$l['stylesheet_updates'] = "Stylesheets Check";
+$l['plugin_updates'] = "Plugins Update";
+$l['sidebar'] = "Updates";
+
+$l['can_updates_stylesheet'] = "Can add plugin stylesheets to the master style.?";
+$l['can_updates_plugin'] = "Can perform plugin updates?";
